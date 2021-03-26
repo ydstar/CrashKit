3a488998edefe2036b0ec111a50dfd9fcb5ad328
侯亚东 <hydznsqk@163.com>
2021-03-26 14:44:57 +0800

    Initial commit

--- crash-kit/src/main/java/com/crash/kit/CrashMgr.kt
+++ crash-kit/src/main/java/com/crash/kit/CrashMgr.kt
@@ -2,24 +2,18 @@ package com.crash.kit
 
 import com.crash.kit.util.ActivityManager
 import com.crash.kit.util.AppGlobals
-import org.devio.`as`.proj.libbreakpad.NativeCrashHandler
 import java.io.File
 
 /**
- * 捕获java层和native层的crash,然后存到本地文件中
+ * 捕获java层的crash,然后存到本地文件中
  */
 object CrashMgr {
     private const val CRASH_DIR_JAVA = "java_crash"
-    private const val CRASH_DIR_NATIVE = "native_crash"
 
     fun init() {
         ActivityManager.instance.init(AppGlobals.get()!!)
-
         val javaCrashDir = getJavaCrashDir()
-        val nativeCrashDir = getNativeCrashDir()
-
         CrashHandler.init(javaCrashDir.absolutePath)
-        NativeCrashHandler.init(nativeCrashDir.absolutePath)
     }
 
     private fun getJavaCrashDir(): File {
@@ -30,20 +24,11 @@ object CrashMgr {
         return javaCrashFile
     }
 
-    private fun getNativeCrashDir(): File {
-        val nativeCrashFile = File(AppGlobals.get()!!.cacheDir, CRASH_DIR_NATIVE)
-        if (!nativeCrashFile.exists()) {
-            nativeCrashFile.mkdirs()
-        }
-        return nativeCrashFile
-    }
-
-
     /**
      * 获取所有的崩溃日志
      */
     fun crashFiles(): Array<File> {
-        return getJavaCrashDir().listFiles() + getNativeCrashDir().listFiles()
+        return getJavaCrashDir().listFiles()
     }
 
     /**
@@ -60,18 +45,6 @@ object CrashMgr {
                 }
             } else {
                 javaCrashFile.delete()
-            }
-        }
-        //删除native层崩溃日志
-        val nativeCrashFile = File(AppGlobals.get()!!.cacheDir, CRASH_DIR_NATIVE)
-        if (nativeCrashFile.exists()) {
-            if (nativeCrashFile.isDirectory) {
-                val listFiles = nativeCrashFile.listFiles()
-                for (file in listFiles) {
-                    file.delete()
-                }
-            } else {
-                nativeCrashFile.delete()
             }
         }
     }
